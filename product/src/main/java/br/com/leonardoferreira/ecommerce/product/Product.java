ff18d027987b76173b6817a3532c8b1a507f2eab
Leonardo Ferreira <mail@leonardoferreira.com.br>
2018-08-22 15:42:12 -0300

    product service

---
+++ product/src/main/java/br/com/leonardoferreira/ecommerce/product/Product.java
@@ -0,0 +1,14 @@
+package br.com.leonardoferreira.ecommerce.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+
+    private Long id;
+
+    private String name;
+
+}
