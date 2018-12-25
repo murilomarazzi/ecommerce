8d76075323853231c46721dd926107eb1eac213f
Leonardo Ferreira <mail@leonardoferreira.com.br>
2018-12-25 19:41:56 -0200

    service discovery pmd

---
+++ servicediscovery/README.md
@@ -0,0 +1,6 @@
+# Service Discover
+
+## Stack
+
+- Java 11
+- SpringBoot 2.1.1
--- servicediscovery/src/main/java/br/com/leonardoferreira/ecommerce/servicediscovery/Application.java
+++ servicediscovery/src/main/java/br/com/leonardoferreira/ecommerce/servicediscovery/Application.java
@@ -1,14 +1,22 @@
 package br.com.leonardoferreira.ecommerce.servicediscovery;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public Sampler sampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
 }
