bfd71682db35763b4be70f4bbb9d7f228cb5b8a8
Leonardo Ferreira <mail@leonardoferreira.com.br>
2018-12-25 19:49:34 -0200

    codestyle

--- apigateway/src/main/java/br/com/leonardoferreira/ecommerce/apigateway/Application.java
+++ apigateway/src/main/java/br/com/leonardoferreira/ecommerce/apigateway/Application.java
@@ -1,10 +1,12 @@
 package br.com.leonardoferreira.ecommerce.apigateway;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableOAuth2Sso
 @EnableZuulProxy
@@ -12,8 +14,13 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Sampler sampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 
 }
